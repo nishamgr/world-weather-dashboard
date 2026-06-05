4f43ec75f3225eb9176e1b5d853cf08aaf4e2283
nishamgr <nmagar@mail.sfsu.edu>
2026-06-05 14:11:36 -0700

    small fix

---
+++ src/clean_weather.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+#read the raw_weather data
+df = pd.read_csv("data/raw_weather.csv")
+
+print("This is before cleaning:")
+print(df.shape)
+
+#removes any dupes rows
+df = df.drop_duplicates()
+
+print("This is after removing dups")
+print(df.shape)
+
+#transformation to remove extra spaces from cities name
+df["City"] = df["City"].str.strip()
+print("This is after cleaning:")
+print(df.shape)
+
+#To keep rows with only cities names
+df = df[df["City"].notna()]
+
+print("This is after cleaning:")
+print(df.shape)
+
+#removing missing vals
+#df = df.dropna()
+#print("After removing missing vals:")
+
+#saves the clean data
+df.to_csv("data/cleaned_weather.csv", index=False)
+print("Finished!")
---
+++ src/scrape_weather.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.timeanddate.com/weather/")
+
+time.sleep(4)
+#data scraping
+
+#cities = driver.find_elements(
+#    By.CLASS_NAME, "my-city_item"
+#)
+
+weather_data = []
+
+#find the rows in the weather table/ prints num of rows sfound
+rows = driver.find_elements(By.CSS_SELECTOR, "table.zebra.tb-theme tbody tr")
+print("Cities found:", len(rows))
+
+#looping thru each row
+for row in rows:
+    cols = row.find_elements(By.TAG_NAME, "td")
+    
+    #checking rows/extracting text
+    if len(cols) >= 3:
+        city = cols[0].text
+        weather = cols[1].text
+        temp = cols[2].text
+        
+        weather_data.append([city, weather, temp])
+
+#append rows to weather_data
+df = pd.DataFrame(
+    weather_data,
+    columns=["City", "Weather_Condition", "Temperature"]
+)
+#Importing to CSV
+df.to_csv("data/raw_weather.csv", index = False)
+
+driver.quit()
+
